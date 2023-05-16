2d00a01d7ee5b89a9f55369f2929a727b5f28b0e
neurips2023-userXXX <133810514+neurips2023-userXXX@users.noreply.github.com>
2023-05-16 15:04:52 -0700

    Update test_retriever.py

--- tests/test_retriever.py
+++ tests/test_retriever.py
@@ -11,8 +11,10 @@ def test_bm25_retriever():
     print(" ------------------ ")
 
 
-# def test_dense_retriever():
-#     retriever = RetrieverWithEmbedding("data/virtualhome/v0/functions", top_k=10)
-#     candidate_documents = retriever("Apply lotion on agent face")
-#     print("".join(candidate_documents))
-#     print(" ------------------ ")
+def test_dense_retriever():
+    # No dataset's prepared in this way for now
+    return
+    retriever = RetrieverWithEmbedding("data/virtualhome/v0/functions", top_k=10)
+    candidate_documents = retriever("Apply lotion on agent face")
+    print("".join(candidate_documents))
+    print(" ------------------ ")
